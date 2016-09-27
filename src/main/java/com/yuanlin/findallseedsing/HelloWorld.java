cf788168ddab66b51ee0443506b7bfbe7554a748
JingEric <yugefly@sina.com>
2016-09-27 14:06:17 +0800

    测试 单例上传

--- src/main/java/com/yuanlin/findallseedsing/HelloWorld.java
+++ src/main/java/com/yuanlin/findallseedsing/HelloWorld.java
@@ -4,4 +4,18 @@ package com.yuanlin.findallseedsing;
  * Created by Administrator on 2016/9/27 0027.
  */
 public class HelloWorld {
+    private HelloWorld(){
+
+    }
+
+    private static  HelloWorld instance = null;
+
+    public synchronized  static HelloWorld getinstance(){
+        if (instance==null){
+            instance = new HelloWorld();
+        }
+        return  instance;
+    }
+
+
 }
